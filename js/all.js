b11f42a0748a9bc305f128e91ede9f66fdedeec4
SuSuy <suyuan.@139.com>
2016-03-15 11:46:52 +0800

    change the content

--- js/all.js
+++ js/all.js
@@ -21,11 +21,11 @@ var arr=[{Type:'电商作品',Name:'双十一电商作品',date:'2015-11'},
 		*Name:名称
 		*date:链接地址
 		 */
-var AboutArr=[{Introduce:'蓝海世纪影业立学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'天猫双十一',url:'http://www.myyqq.cn'},
-	{Introduce:'以宽阔的眼界突破角落的思维，学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'天猫双十二',url:'http://www.baidu.com'},
-	{Introduce:'西部，争鸣全国，以宽阔的眼界突破角落的思维，学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'天猫双十三',url:'http://www.youku.com'},
-	{Introduce:'海世纪影业立足重庆，志在西部，争鸣全国，以宽阔的眼界突破角落的思维，学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'天猫双十四',url:'http://www.taobao.com'},
-	{Introduce:'蓝宽阔的眼界突破角落的思维，学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'天猫双十五',url:'http://www.myyqq.cn'}]
+var AboutArr=[{Introduce:'蓝海世纪影业立学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'双十一',url:'http://www.myyqq.cn'},
+	{Introduce:'以宽阔的眼界突破角落的思维，学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'双十二',url:'http://www.baidu.com'},
+	{Introduce:'西部，争鸣全国，以宽阔的眼界突破角落的思维，学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'双十三',url:'http://www.youku.com'},
+	{Introduce:'海世纪影业立足重庆，志在西部，争鸣全国，以宽阔的眼界突破角落的思维，学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'双十四',url:'http://www.taobao.com'},
+	{Introduce:'蓝宽阔的眼界突破角落的思维，学习发达国家电影工业化产业发展的模式和经验，结合中国实践',Type:'WEB DESIGN',Name:'双十五',url:'http://www.myyqq.cn'}]
 		/*****************************************************************AboutArr		content页面的内容；
 		*Introduce:介绍内容
 		*Type:类型
@@ -37,19 +37,19 @@ var arrAbout=[
 			{
 				Time:'2015年9月—2016年12月',
 				Company:'天天向上天天向上×××××',
-				Job:'电商设计',
+				Job:'前端开发',
 				Duty:'负责天猫店铺设××××等等我们拥有西南乃至全国领先的设计团队，并在互联网端为您提供顶尖行业的呈现方式，我们采用业界领先的HTML5+CSS3技术，为您呈现出富有视觉动感与商业价值并存的桌面端'
 			},
 			{
 				Time:'2015年9月—2016年12月',
 				Company:'天天向上天天向上×××××',
-				Job:'电商设计',
+				Job:'前端开发',
 				Duty:'负责天猫店铺设××××等等我们拥有西南乃至全国领先的设计团队，并在互联网端为您提供顶尖行业的呈现方式，我们采用业界领先的HTML5+CSS3技术，为您呈现出富有视觉动感与商业价值并存的桌面端'
 			},
 			{
 				Time:'2015年9月—2016年12月',
 				Company:'天天向上天天向上×××××',
-				Job:'电商设计',
+				Job:'前端开发',
 				Duty:'负责天猫店铺设××××等等我们拥有西南乃至全国领先的设计团队，并在互联网端为您提供顶尖行业的呈现方式，我们采用业界领先的HTML5+CSS3技术，为您呈现出富有视觉动感与商业价值并存的桌面端'
 			}]
 		/*****************************************************************************arrAbout		about页面的内容；
